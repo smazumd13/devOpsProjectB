f2b62f250b447ebe5a067b8c1f6ab17bac167943
sourav mazumdar <sourav.mazumdar@bell.ca>
2021-11-21 20:04:51 +0530

    removed storage account vars

--- terraform/Vars.tf
+++ terraform/Vars.tf
@@ -19,36 +19,9 @@ variable "appName" {
     default = "smprojB"
 }
 
-variable "acrName" {
-    type = string
-    default = "myconreg1304"
-}
-
 variable "rgroupName" {
     type = string
     default = "projectb-rg"
-}
-
-variable "sqlAdminuser" {
-    type = string
-    description = "Enter username for db admin"
-    sensitive = true
-}
-
-variable "sqlAdminPass" {
-    type = string
-    description = "Enter password for db admin"
-    sensitive = true
-}
-
-variable "dbName" {
-    type = string
-    default = "smsqldb13"
-}
-
-variable "sqlserverName" {
-    type = string
-    default = "smsqlserv13"
 }
 
 variable "clusterPrefix" {
